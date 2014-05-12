e796b68fc1b2bfc4b1d28fec02fa77de9be166c3
Boris <Boris@boriss-macbook.home>
2014-05-12 14:47:14 +0200

    Orchesermitarbeiter Ronald Janezic angelegt

--- src/main/java/at/roomplanning/App.java
+++ src/main/java/at/roomplanning/App.java
@@ -182,6 +182,12 @@ public class App {
 		calendar.set(1987, 10, 5);
 		JosefReif.setBirthDate(calendar.getTime());
 		
+		Employee RonaldJanezic = new Employee();
+		RonaldJanezic.setFirstName("Ronald");
+		JosefReif.setFamilyName("Janezic");
+		calendar.set(1987, 10, 5);
+		RonaldJanezic.setBirthDate(calendar.getTime());
+		
 		
 		// Rolle/Funktion erstellen
 
