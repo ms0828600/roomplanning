af83f386bb089354c267587d0dea17b4634dcfd2
Adel Gadllah <adel.gadllah@gmail.com>
2014-09-07 10:48:04 +0200

    Add AlgorithmGroupRepository

---
+++ src/main/java/roomplanning/db/dao/AlgorithmGroupRepository.java
@@ -0,0 +1,9 @@
+package roomplanning.db.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import roomplanning.db.model.AlgorithmGroup;
+
+public interface AlgorithmGroupRepository extends CrudRepository<AlgorithmGroup, Long>{
+
+}
